9c3dc3c5c8c26ab2c45810ae2125ca9932b1c1fe
Jake Solomon <jake.ch.solomon@gmail.com>
2026-01-30 16:26:12 -0500

    - Remove unused content - Re-wrote experience descriptions - Removed unused packages - Updated README

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { MantineProvider } from '@mantine/core'
-import { Analytics } from "@vercel/analytics/react"
 import LogRocket from 'logrocket';
 import '@mantine/core/styles.css'
-import '@mantine/carousel/styles.css'
 import { theme } from './theme'
 import App from './App.tsx'
 
 if (import.meta.env.PROD) {
-  LogRocket.init('j5epm2/jakewantstoworkatbuffer');
+  LogRocket.init('j5epm2/portfolio-site');
 }
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Analytics/>
     <MantineProvider theme={theme}>
       <App />
     </MantineProvider>
